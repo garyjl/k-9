thunderbird/thunderbird-android

commit a2674efff1155228b31d3a7b966624b2ebed19f7

compose: small fixes for alternate recipient adapter
this slightly alters behavior when a non-contact address is picked, to
make sure a crypto status is shown in the alternate dropdown.

k9mail/src/main/java/com/fsck/k9/view/RecipientSelectView.java
@@ -334,8 +334,7 @@ public class RecipientSelectView extends TokenCompleteTextView<Recipient> implem
                 if (contactLookupUri != null) {
                     return new RecipientLoader(getContext(), cryptoProvider, contactLookupUri, true);
                 } else {
-                    String address = alternatesPopupRecipient.address.getAddress();
-                    return new RecipientLoader(getContext(), cryptoProvider, address);
+                    return new RecipientLoader(getContext(), cryptoProvider, alternatesPopupRecipient.address);
                 }
             }
         }
@@ -451,7 +450,7 @@ public class RecipientSelectView extends TokenCompleteTextView<Recipient> implem
 
         RecipientTokenSpan[] recipientSpans = text.getSpans(0, text.length(), RecipientTokenSpan.class);
         for (RecipientTokenSpan recipientSpan : recipientSpans) {
-            if (recipientSpan.getToken() == currentRecipient) {
+            if (recipientSpan.getToken().equals(currentRecipient)) {
                 return recipientSpan.view;
             }
         }

k9mail/src/main/java/com/fsck/k9/activity/AlternateRecipientAdapter.java
@@ -49,6 +49,10 @@ public class AlternateRecipientAdapter extends BaseAdapter {
 
     public void setAlternateRecipientInfo(List<Recipient> recipients) {
         this.recipients = recipients;
+        int indexOfCurrentRecipient = recipients.indexOf(currentRecipient);
+        if (indexOfCurrentRecipient >= 0) {
+            currentRecipient = recipients.get(indexOfCurrentRecipient);
+        }
         recipients.remove(currentRecipient);
         notifyDataSetChanged();
     }

garyjl/k-9

commit 5bce3ea1493eaeecd3810784dc0217af70aa941f

fix broken referencing of recipient token in RecipientSelectView
Passing through the actual recipient object was broken in
a2674efff1155228b31d3a7b966624b2ebed19f7. I changed the code to no
longer rely on the actual object being passed through correctly, but
just one that fulfills `equals`.

k9mail/src/main/java/com/fsck/k9/view/RecipientSelectView.java
@@ -401,8 +401,16 @@ public class RecipientSelectView extends TokenCompleteTextView<Recipient> implem
     }
 
     @Override
-    public void onRecipientChange(Recipient currentRecipient, Recipient alternateAddress) {
+    public void onRecipientChange(Recipient recipientToReplace, Recipient alternateAddress) {
         alternatesPopup.dismiss();
+
+        List<Recipient> currentRecipients = getObjects();
+        int indexOfRecipient = currentRecipients.indexOf(alternateAddress);
+        if (indexOfRecipient == -1) {
+            Log.e(K9.LOG_TAG, "Tried to refresh invalid view token!");
+            return;
+        }
+        Recipient currentRecipient = currentRecipients.get(indexOfRecipient);
 
         currentRecipient.address = alternateAddress.address;
         currentRecipient.addressLabel = alternateAddress.addressLabel;
@@ -630,5 +638,6 @@ public class RecipientSelectView extends TokenCompleteTextView<Recipient> implem
                 photoThumbnailUri = Uri.parse(uriString);
             }
         }
+
     }
 }